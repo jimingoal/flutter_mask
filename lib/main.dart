bfaf028e29d70bfa22463eb7c8a6898f6c750be8
jimingoal <jimingoal@gmail.com>
2020-09-08 15:28:16 +0900

    리스트 화면 출력

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'model/store.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,6 +32,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Store> stores = [];
+
   Future fetch() async {
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat= 37.631947&lng=127.0180892';
@@ -36,17 +41,31 @@ class _MyHomePageState extends State<MyHomePage> {
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+
+    final jsonResult = jsonDecode(response.body);
+
+    final jsonStores = jsonResult['stores'];
+
+    stores.clear();
+
+    jsonStores.forEach((e) {
+      final store = Store.fromJson(e);
+      stores.add(store);
+    });
+
+    print(stores);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: RaisedButton(
-          child: Text('테스트'),
-          onPressed: fetch,
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: fetch)],
+      ),
+      body: ListView(
+        children: stores.map((e) => Text(e.name ?? '')).toList(),
+      ),
+    );
   }
 }
