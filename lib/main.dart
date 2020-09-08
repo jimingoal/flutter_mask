fdbe6e16bfa67a9c42560daaf81136ae97eaff10
jimingoal <jimingoal@gmail.com>
2020-09-08 18:58:17 +0900

    repository 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_mask/repository/store_repository.dart';
 import 'model/store.dart';
 
 void main() {
@@ -35,58 +33,48 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<Store> stores = [];
   var isLoading = true;
 
-  Future fetch() async {
-    setState(() {
-      isLoading = true;
-    });
-    var url =
-        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat= 37.631947&lng=127.0180892';
-
-    var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-
-    final jsonResult = jsonDecode(response.body);
-
-    final jsonStores = jsonResult['stores'];
-
-    setState(() {
-      stores.clear();
-
-      jsonStores.forEach((e) {
-        final store = Store.fromJson(e);
-        stores.add(store);
-      });
-      isLoading = false;
-    });
-
-    print("새로고침 완료");
-  }
+  StoreRepository storeRepository = StoreRepository();
 
   @override
   void initState() {
     super.initState();
-    fetch();
+    storeRepository.fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 재고 있는곳: ${stores.length}'),
-        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: fetch)],
+        title: Text('마스크 재고 있는곳: ${stores.where((e) {
+          return e.remainStat == 'plenty' ||
+              e.remainStat == 'some' ||
+              e.remainStat == 'few';
+        }).length}'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                storeRepository.fetch();
+              })
+        ],
       ),
-      body: isLoading
-          ? loadingWidget()
-          : ListView(
-              children: stores.map((e) {
-                return ListTile(
-                  title: Text(e.name),
-                  subtitle: Text(e.addr),
-                  trailing: _buildReaminStat(e),
-                );
-              }).toList(),
-            ),
+      body: Center(
+        child: isLoading
+            ? loadingWidget()
+            : ListView(
+                children: stores.where((e) {
+                  return e.remainStat == 'plenty' ||
+                      e.remainStat == 'some' ||
+                      e.remainStat == 'few';
+                }).map((e) {
+                  return ListTile(
+                    title: Text(e.name),
+                    subtitle: Text(e.addr),
+                    trailing: _buildReaminStat(e),
+                  );
+                }).toList(),
+              ),
+      ),
     );
   }
 
---
+++ lib/repository/store_repository.dart
@@ -0,0 +1,33 @@
+import '../model/store.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class StoreRepository {
+  Future fetch() async {
+    final List<Store> stores = [];
+
+    // setState(() {
+    //   isLoading = true;
+    // });
+    var url =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat= 37.631947&lng=127.0180892';
+
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    final jsonResult = jsonDecode(response.body);
+
+    final jsonStores = jsonResult['stores'];
+
+    jsonStores.forEach((e) {
+      final store = Store.fromJson(e);
+      stores.add(store);
+    });
+    // isLoading = false;
+
+    print("새로고침 완료");
+
+    return stores;
+  }
+}
