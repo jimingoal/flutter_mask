034bc4abf8c6183676b840fc86bf9f5e0c4df95a
jimingoal <jimingoal@gmail.com>
2020-09-08 16:37:32 +0900

    재고 수량별 표시

--- lib/main.dart
+++ lib/main.dart
@@ -83,10 +83,59 @@ class _MyHomePageState extends State<MyHomePage> {
                 return ListTile(
                   title: Text(e.name),
                   subtitle: Text(e.addr),
-                  trailing: Text(e.remainStat ?? ''),
+                  trailing: _buildReaminStat(e),
                 );
               }).toList(),
             ),
+    );
+  }
+
+  Widget _buildReaminStat(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+
+    print(store.remainStat);
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+    }
+
+    return Column(
+      children: [
+        Text(
+          remainStat,
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          description,
+          style: TextStyle(
+            color: color,
+          ),
+        ),
+      ],
     );
   }
 
